b94373455e38fc46607685a4e9125562ac61b3cb
JayJay1024 <lygm1062@gmail.com>
2022-03-03 02:14:23 +0800

    style: connect wallet modal

--- src/components/widget/ConnectPolkadot.tsx
+++ src/components/widget/ConnectPolkadot.tsx
@@ -2,10 +2,11 @@ import { Button, Modal } from 'antd';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAccount, useApi } from '../../hooks';
+import { CloseIcon } from '../icons';
 
 export function ConnectPolkadot() {
   const { t } = useTranslation();
-  const [isHelperModalVisible, setIsHelpModalVisible] = useState(false);
+  const [isHelperModalVisible, setIsHelpModalVisible] = useState(true);
   const { account } = useAccount();
   const { network, connection, connectNetwork } = useApi();
 
@@ -25,6 +26,7 @@ export function ConnectPolkadot() {
       </Button>
 
       <Modal
+        closeIcon={<CloseIcon />}
         title={t('Connect to Polkadot')}
         visible={isHelperModalVisible}
         footer={null}
@@ -33,13 +35,13 @@ export function ConnectPolkadot() {
         <div className="flex flex-col items-center">
           <p>{t('You are trying to connect to Polkadot extension, please authorize in the extension.')}</p>
 
-          <div className="flex justify-between items-center p-4 w-full rounded-xl mt-4 border border-gray-200">
-            <b>polkadot.js</b>
-            <img src="/image/polkadot.svg" style={{ height: 32 }} alt="" />
+          <div className="flex items-center p-4 w-full rounded-xl mt-4 border border-gray-200">
+            <img src="/image/polkadot.svg" style={{ height: 44 }} alt="" />
+            <b className="ml-3 font-medium text-sm text-black opacity-80">{`polkadot{.js} extension`}</b>
           </div>
 
           <Button
-            className="mt-4 self-end"
+            className="mt-4 w-2/5"
             onClick={() => {
               window.open('https://polkadot.js.org', 'blank');
             }}
