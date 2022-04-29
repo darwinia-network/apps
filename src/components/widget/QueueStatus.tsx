f78b976a4e031c054bc55aa548efe2e573b4c9b4
JayJay1024 <lygm1062@gmail.com>
2022-04-29 17:19:14 +0800

    chore: handle extrinsic success or fail status

--- src/components/widget/QueueStatus.tsx
+++ src/components/widget/QueueStatus.tsx
@@ -1,15 +1,18 @@
-import { notification } from 'antd';
+import { Typography, notification } from 'antd';
 import { SyncOutlined } from '@ant-design/icons';
 import { Trans } from 'react-i18next';
-import { useEffect } from 'react';
-import { useQueue } from '../../hooks';
+import { useEffect, ReactElement } from 'react';
+import { SubmittableResult } from '@polkadot/api';
+import { useQueue, useApi } from '../../hooks';
+import { SubscanLink } from './SubscanLink';
 
 export const QueueStatus = () => {
   const { txqueue } = useQueue();
+  const { network } = useApi();
 
   useEffect(() => {
     // eslint-disable-next-line complexity
-    txqueue.forEach(({ error, status, extrinsic, rpc, id }) => {
+    txqueue.forEach(({ error, status, extrinsic, rpc, id, result }) => {
       let { method, section } = rpc;
       if (extrinsic) {
         const found = extrinsic.registry.findMetaCall(extrinsic.callIndex);
@@ -41,20 +44,49 @@ export const QueueStatus = () => {
           description: <Trans>Has been broadcast, waiting for the node to receive</Trans>,
           icon: <SyncOutlined spin />,
         });
-      } else if (status === 'inblock') {
-        notification.info({
-          ...config,
-          description: <Trans>The transaction has been packaged</Trans>,
-          icon: <SyncOutlined spin />,
-        });
       } else if (status === 'cancelled') {
         notification.warning({
           ...config,
           description: <Trans>The transaction has been cancelled</Trans>,
         });
+      } else if (status === 'finalized' || status === 'inblock') {
+        (result as SubmittableResult).events
+          .filter(({ event: { section } }) => section === 'system')
+          .forEach(({ event: { method } }): void => {
+            const notice = (type: 'success' | 'error', message: ReactElement, txHash: string) =>
+              notification[type]({
+                ...config,
+                description: (
+                  <div>
+                    <p>{message}</p>
+                    <SubscanLink network={network.name} txHash={txHash}>
+                      <Typography.Text copyable underline>
+                        {txHash}
+                      </Typography.Text>
+                    </SubscanLink>
+                  </div>
+                ),
+              });
+
+            if (method === 'ExtrinsicFailed') {
+              notice(
+                'error',
+                <Trans>The transaction has been failed, you can check the transaction in the explorer.</Trans>,
+                (result as SubmittableResult).txHash.toHex()
+              );
+            } else if (method === 'ExtrinsicSuccess') {
+              notice(
+                'success',
+                <Trans>
+                  The transaction has been sent, please check the transaction progress in the history or explorer.
+                </Trans>,
+                (result as SubmittableResult).txHash.toHex()
+              );
+            }
+          });
       }
     });
-  }, [txqueue]);
+  }, [txqueue, network.name]);
 
   return null;
 };
