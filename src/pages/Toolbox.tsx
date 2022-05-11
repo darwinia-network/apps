67e83c14d459321bb3bbd38edde943c892782a8d
JayJay1024 <lygm1062@gmail.com>
2022-05-11 10:29:55 +0800

    chore: toolbox dvm address display only on crab and pangolin

--- src/pages/Toolbox.tsx
+++ src/pages/Toolbox.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Tabs, Typography, Alert } from 'antd';
+import { Form, Input, Tabs, Typography } from 'antd';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { withRouter } from 'react-router-dom';
@@ -22,49 +22,48 @@ function Page() {
       <Tabs
         className={`lg:px-8 px-4 w-full mx-auto dark:shadow-none dark:border-transparent pb-5 page-account-tabs page-account-tabs-${name}`}
       >
-        <Tabs.TabPane tab={t('DVM Address')} key="address">
-          <Alert
-            message={t(`Darwinia will support DVM soon, but please don't transfer assets to this address now`)}
-            type="warning"
-            className="mb-2"
-          />
-          <Form layout="vertical">
-            <Form.Item
-              label={<Label text={t('DVM account')} info={t('DVM format account ID to Darwinia network account ID')} />}
-              name="address"
-              rules={[
-                {
-                  validator(_, value) {
-                    return web3.utils.isAddress(value) ? Promise.resolve() : Promise.reject();
+        {(name === 'crab' || name === 'pangolin') && (
+          <Tabs.TabPane tab={t('DVM Address')} key="address">
+            <Form layout="vertical">
+              <Form.Item
+                label={
+                  <Label text={t('DVM account')} info={t('DVM format account ID to Darwinia network account ID')} />
+                }
+                name="address"
+                rules={[
+                  {
+                    validator(_, value) {
+                      return web3.utils.isAddress(value) ? Promise.resolve() : Promise.reject();
+                    },
+                    message: t('Address Invalid'),
                   },
-                  message: t('Address Invalid'),
-                },
-              ]}
-            >
-              <Input
-                onChange={(event) => {
-                  const value = event.target.value;
-                  const addr = web3.utils.isAddress(value) ? value : '';
+                ]}
+              >
+                <Input
+                  onChange={(event) => {
+                    const value = event.target.value;
+                    const addr = web3.utils.isAddress(value) ? value : '';
 
-                  setAddress(addr);
-                }}
-                placeholder={t('DVM format account e.g.')}
-                allowClear
-                size="large"
-              />
-            </Form.Item>
-
-            {address && (
-              <Form.Item label={t('The resulting darwinia network account id is')}>
-                <div className="bg-white w-full rounded-lg p-4">
-                  <Typography.Text copyable>
-                    {convertToSS58(dvmAddressToAccountId(address).toString(), ss58Prefix)}
-                  </Typography.Text>
-                </div>
+                    setAddress(addr);
+                  }}
+                  placeholder={t('DVM format account e.g.')}
+                  allowClear
+                  size="large"
+                />
               </Form.Item>
-            )}
-          </Form>
-        </Tabs.TabPane>
+
+              {address && (
+                <Form.Item label={t('The resulting darwinia network account id is')}>
+                  <div className="bg-white w-full rounded-lg p-4">
+                    <Typography.Text copyable>
+                      {convertToSS58(dvmAddressToAccountId(address).toString(), ss58Prefix)}
+                    </Typography.Text>
+                  </div>
+                </Form.Item>
+              )}
+            </Form>
+          </Tabs.TabPane>
+        )}
 
         <Tabs.TabPane tab={t('DVM Withdraw')} key="withdraw" disabled={name === 'pangoro' || name === 'darwinia'}>
           <Withdraw />
