780f324c6c19c25ceafe53851e017b435e230923
JayJay1024 <lygm1062@gmail.com>
2022-05-20 16:39:10 +0800

    fix: update staking record page size

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -13,7 +13,7 @@ export const DATE_TIME_FORMATE = 'yyyy/MM/dd HH:mm:ss';
 
 export const ETHEREUM_CLAIM_DEPOSIT = '0x649fdf6ee483a96e020b889571e93700fbd82d88';
 
-export const STAKING_RECORD_PAGE_SIZE = 2;
+export const STAKING_RECORD_PAGE_SIZE = 10;
 
 // eslint-disable-next-line no-magic-numbers
 export const DARWINIA_UNBONDING_PERIOD = 14 * 24 * 60 * 60 * 1000;
