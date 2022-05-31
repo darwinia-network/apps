1a2683365ed233a4f66ef9e65969a2a0b57fe2f1
JayJay1024 <lygm1062@gmail.com>
2022-05-31 11:46:05 +0800

    chore: add kton abi

--- src/config/abi/index.ts
+++ src/config/abi/index.ts
@@ -1,10 +1,12 @@
 import { AbiItem } from 'web3-utils';
 import bankABI from './bankABI.json';
+import ktonABI from './ktonABI.json';
 
-type keys = 'bankABI';
+type keys = 'bankABI' | 'ktonABI';
 
 export const abi = {
   bankABI,
+  ktonABI,
 } as {
   [key in keys]: AbiItem[];
 };
