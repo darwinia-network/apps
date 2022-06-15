e7fd8602049740c2cee3f1eea7a98c7fcdff152c
JayJay1024 <lygm1062@gmail.com>
2022-06-15 13:13:40 +0800

    feat: check brower helper

--- src/utils/helper/common.ts
+++ src/utils/helper/common.ts
@@ -80,3 +80,31 @@ export function buf2hex(buffer: ArrayBuffer) {
   // eslint-disable-next-line no-magic-numbers
   return '0x' + Array.prototype.map.call(new Uint8Array(buffer), (x) => ('00' + x.toString(16)).slice(-2)).join('');
 }
+
+// eslint-disable-next-line complexity
+export const checkBrower = () => {
+  const userAgent = window.navigator.userAgent;
+
+  // The order matters here, and this may report false positives for unlisted browsers.
+  if (userAgent.indexOf('Firefox') > -1) {
+    return 'Mozilla Firefox';
+    // 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'
+  } else if (userAgent.indexOf('Opera') > -1 || userAgent.indexOf('OPR') > -1) {
+    return 'Opera';
+    // 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106'
+  } else if (userAgent.indexOf('Trident') > -1) {
+    return 'Microsoft Internet Explorer';
+    // 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko'
+  } else if (userAgent.indexOf('Edge') > -1) {
+    return 'Microsoft Edge';
+    // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
+  } else if (userAgent.indexOf('Chrome') > -1) {
+    return 'Google Chrome or Chromium';
+    // 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36'
+  } else if (userAgent.indexOf('Safari') > -1) {
+    return 'Apple Safari';
+    // 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306'
+  } else {
+    return 'Unknown';
+  }
+};
