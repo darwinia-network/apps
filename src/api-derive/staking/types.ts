c439f28f6b8991ff4ea23e442dc93f804effe1d7
JayJay1024 <lygm1062@gmail.com>
2022-06-23 17:29:05 +0800

    feat: staking lock records

--- src/api-derive/staking/types.ts
+++ src/api-derive/staking/types.ts
@@ -2,7 +2,7 @@ import { Exposure } from '@darwinia/types';
 import { StakingLedgerT as StakingLedger } from '@darwinia/types/interfaces';
 import { TimeDepositItem } from '@darwinia/types/interfaces/darwiniaInject';
 import { DeriveStakingKeys } from '@polkadot/api-derive/staking/types';
-import {
+import type {
   AccountId,
   Balance,
   BlockNumber,
@@ -12,6 +12,7 @@ import {
   ValidatorPrefs,
 } from '@polkadot/types/interfaces';
 import type { BN } from '@polkadot/util';
+import type { Struct, Compact, Vec, u64 } from '@polkadot/types';
 
 export interface DeriveStakerReward {
   era: EraIndex;
@@ -88,3 +89,32 @@ type DeriveUnlocking = {
   remainingEras: BN;
   value: Balance;
 };
+
+export type TsInMs = u64;
+
+export interface DarwiniaStakingStructsTimeDepositItem extends Struct {
+  readonly value: Compact<Balance>;
+  readonly startTime: Compact<TsInMs>;
+  readonly expireTime: Compact<TsInMs>;
+}
+
+export interface DarwiniaSupportStructsUnbonding extends Struct {
+  readonly amount: Balance;
+  readonly until: BlockNumber;
+}
+
+export interface DarwiniaSupportStructsStakingLock extends Struct {
+  readonly stakingAmount: Balance;
+  readonly unbondings: Vec<DarwiniaSupportStructsUnbonding>;
+}
+
+export interface DarwiniaStakingStructsStakingLedger extends Struct {
+  readonly stash: AccountId;
+  readonly active: Compact<Balance>;
+  readonly activeDepositRing: Compact<Balance>;
+  readonly activeKton: Compact<Balance>;
+  readonly depositItems: Vec<DarwiniaStakingStructsTimeDepositItem>;
+  readonly ringStakingLock: DarwiniaSupportStructsStakingLock;
+  readonly ktonStakingLock: DarwiniaSupportStructsStakingLock;
+  readonly claimedRewards: Vec<EraIndex>;
+}
--- src/components/account/StakingHistory.tsx
+++ src/components/account/StakingHistory.tsx
@@ -1,29 +1,71 @@
 import { Card, Tabs } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { AccountStatus } from '../../model';
+import type { Option } from '@polkadot/types';
 import { AccountHistoryProps } from '../staking/interface';
-import { useApi } from '../../hooks';
+import { useApi, useAccount } from '../../hooks';
 import { CustomTab } from '../widget/CustomTab';
+import type { DarwiniaStakingStructsStakingLedger } from '../../api-derive/types';
 import { BondRecords } from './BondRecords';
 import { UnbondRecords } from './UnbondRecords';
+import { LockedRecords } from './LockedRecords';
+
+enum TabState {
+  LOCKS = 'Locks',
+  UNBONDING = 'Unbonding',
+  UNBONDED = 'Unbonded',
+}
 
 export function StakingHistory({ tokens }: AccountHistoryProps) {
-  const [activeKey, setActiveKey] = useState<AccountStatus>('bonded');
+  const { api, network } = useApi();
+  const { account } = useAccount();
   const { t } = useTranslation();
-  const { network } = useApi();
+  const [activeKey, setActiveKey] = useState(TabState.LOCKS);
+  const [ledger, setLedger] = useState<DarwiniaStakingStructsStakingLedger | null>();
+
+  useEffect(() => {
+    if (!account?.displayAddress) {
+      setLedger(null);
+      return;
+    }
+
+    let unsub: () => void;
+
+    (async () => {
+      unsub = await api.query.staking.ledger(
+        account.displayAddress,
+        (res: Option<DarwiniaStakingStructsStakingLedger>) => {
+          setLedger(res.isSome ? res.unwrap() : null);
+        }
+      );
+    })();
+
+    return () => unsub();
+  }, [api, account?.displayAddress]);
 
   return (
     <Card className="relative shadow-xxl">
       <Tabs
         defaultActiveKey={activeKey}
-        onChange={(key) => setActiveKey(key as AccountStatus)}
+        onChange={(key) => setActiveKey(key as TabState)}
         className={`overflow-x-scroll page-account-tabs page-account-tabs-${network.name}`}
       >
-        <Tabs.TabPane key="bonded" tab={<CustomTab text={t('Bond')} tabKey="bonded" activeKey={activeKey} />}>
+        <Tabs.TabPane
+          key={TabState.LOCKS}
+          tab={<CustomTab text={t(TabState.LOCKS)} tabKey={TabState.LOCKS} activeKey={activeKey} />}
+        >
+          <LockedRecords locks={ledger?.depositItems || []} />
+        </Tabs.TabPane>
+        <Tabs.TabPane
+          key={TabState.UNBONDING}
+          tab={<CustomTab text={t(TabState.UNBONDING)} tabKey={TabState.UNBONDING} activeKey={activeKey} />}
+        >
           <BondRecords tokens={tokens} />
         </Tabs.TabPane>
-        <Tabs.TabPane key="unbonding" tab={<CustomTab text={t('Unbond')} tabKey="unbonding" activeKey={activeKey} />}>
+        <Tabs.TabPane
+          key={TabState.UNBONDED}
+          tab={<CustomTab text={t(TabState.UNBONDED)} tabKey={TabState.UNBONDED} activeKey={activeKey} />}
+        >
           <UnbondRecords />
         </Tabs.TabPane>
       </Tabs>
