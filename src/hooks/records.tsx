72c31e1998ee551fdaf21d528a1c657a3d11332f
JayJay1024 <lygm1062@gmail.com>
2022-07-16 00:36:46 +0800

    chore: update rxGet and rxPost response

--- src/hooks/records.tsx
+++ src/hooks/records.tsx
@@ -1,6 +1,6 @@
 import { useState, useCallback, useEffect } from 'react';
 import { Subscription } from 'rxjs';
-import { RecordsQueryRequest, rxGet } from '../utils';
+import { HttpRequest, rxGet } from '../utils';
 
 export interface RecordsHook<T> {
   loading: boolean;
@@ -10,17 +10,17 @@ export interface RecordsHook<T> {
   refetch?: (...args: any) => Subscription;
 }
 
-export function useRecordsQuery<T = unknown>(req: RecordsQueryRequest): RecordsHook<T> {
+export function useRecordsQuery<T = unknown>(req: HttpRequest): RecordsHook<T> {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Record<string, unknown> | null>(null);
   const [data, setData] = useState<T | null>(null);
 
-  const query = useCallback((request: RecordsQueryRequest) => {
+  const query = useCallback((request: HttpRequest) => {
     setLoading(true);
 
-    return rxGet<T>(request).subscribe({
+    return rxGet<{ data: T | null }>(request).subscribe({
       next: (res) => {
-        setData(res);
+        setData(res.data);
       },
       error: (err) => {
         setError(err);
