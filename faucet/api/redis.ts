bfe495976d51385b6fe48649d03603bd737799c3
JayJay1024 <lygm1062@gmail.com>
2022-07-15 15:17:17 +0800

    chore: check redis url env when init redis client

--- faucet/api/redis.ts
+++ faucet/api/redis.ts
@@ -7,8 +7,12 @@ export function redis(): { client: Redis | null; error: Error | null } {
     return { client: _redis, error: null };
   }
 
+  if (!process.env.REDIS_CONNECT_URL) {
+    return { client: null, error: new Error('Failed to get cache client url') };
+  }
+
   try {
-    _redis = new Redis(process.env.REDIS_CONNECT_URL || '');
+    _redis = new Redis(process.env.REDIS_CONNECT_URL);
     return { client: _redis, error: null };
   } catch (err) {
     const error = err as Error;
