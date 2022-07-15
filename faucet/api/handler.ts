160ecfa28b13f35876a2e77d5a6d81d3d64aa741
JayJay1024 <lygm1062@gmail.com>
2022-07-15 19:56:49 +0800

    chore: update faucet service headers

--- faucet/api/handler.ts
+++ faucet/api/handler.ts
@@ -15,6 +15,21 @@ const HOUR_TO_MILLISECONDS = 60 * 60 * 1000;
 export async function handler(req: VercelRequest, res: VercelResponse, config: Config) {
   res.setHeader('content-type', 'application/json');
 
+  // TODO:
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   try {
     const { client, error } = redis();
     if (!client || error) {
