c0b507e9e22d8af7e7865014c77429a8e6251720
JayJay1024 <lygm1062@gmail.com>
2022-07-15 13:51:54 +0800

    fix: remove seed from config.ts

--- faucet/api/pangolin/config.ts
+++ faucet/api/pangolin/config.ts
@@ -2,7 +2,6 @@ import type { Config } from '../types';
 
 /* eslint-disable no-magic-numbers */
 export const config: Config = {
-  seed: process.env.PANGOLIN_SEED,
   endpoint: 'wss://pangolin-rpc.darwinia.network',
   throttleHours: 12, // 12 hours
   transferMount: 100000000000, // 100 RING
