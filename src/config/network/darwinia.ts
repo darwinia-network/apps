0a055f6a0aae201e289f17990ba828068bb2a7c8
JayJay1024 <lygm1062@gmail.com>
2022-07-20 13:55:13 +0800

    feat: darwinia smart chain

--- src/config/network/darwinia.ts
+++ src/config/network/darwinia.ts
@@ -1,6 +1,23 @@
-import { PolkadotChainConfig } from '../../model';
+import { DVMChainConfig } from '../../model';
 
-export const darwiniaConfig: PolkadotChainConfig = {
+export const darwiniaConfig: DVMChainConfig = {
+  dvm: {
+    ring: {
+      address: '',
+      symbol: 'RING',
+      decimals: 18,
+    },
+  },
+  ethereumChain: {
+    blockExplorerUrls: ['https://darwinia.subscan.io/'],
+    chainId: '46',
+    chainName: 'darwinia',
+    nativeCurrency: {
+      decimals: 18,
+      symbol: 'RING',
+    },
+    rpcUrls: ['https://darwinia-rpc.darwinia.network/'],
+  },
   facade: {
     logo: '/image/network/darwinia.svg',
   },
--- src/pages/Toolbox.tsx
+++ src/pages/Toolbox.tsx
@@ -1,5 +1,5 @@
 import { Form, Input, Tabs, Typography } from 'antd';
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import web3 from 'web3';
 import { Withdraw } from '../components/toolbox/withdraw';
@@ -15,76 +15,71 @@ export function Toolbox() {
     network: { ss58Prefix, name },
   } = useApi();
   const [address, setAddress] = useState('');
-  const displayDvm = useMemo(() => name === 'crab' || name === 'pangolin' || name === 'pangoro', [name]);
 
   return (
     <MetamaskProvider>
       <Tabs
         className={`lg:px-8 px-4 w-full mx-auto dark:shadow-none dark:border-transparent pb-5 page-account-tabs page-account-tabs-${name}`}
       >
-        {displayDvm && (
-          <Tabs.TabPane tab={t('DVM Address')} key="address">
-            <Form layout="vertical" className="max-w-xl">
-              <Form.Item
-                label={
-                  <Label
-                    text={t('DVM account')}
-                    info={
-                      <Trans t={t}>
-                        Ethereum-compatible Smart Chain Address starting with 0x. More details please refer{' '}
-                        <a
-                          rel="noopener noreferrer"
-                          target="_blank"
-                          href="https://darwinianetwork.medium.com/build-on-darwinia-2-1-address-formats-in-darwinia-e964cc91fccc"
-                        >
-                          here
-                        </a>{' '}
-                        .
-                      </Trans>
-                    }
-                  />
-                }
-                name="address"
-                rules={[
-                  {
-                    validator(_, value) {
-                      return web3.utils.isAddress(value) ? Promise.resolve() : Promise.reject();
-                    },
-                    message: t('Address Invalid'),
-                  },
-                ]}
-              >
-                <Input
-                  onChange={(event) => {
-                    const value = event.target.value;
-                    const addr = web3.utils.isAddress(value) ? value : '';
-
-                    setAddress(addr);
-                  }}
-                  placeholder={t('DVM format account e.g.')}
-                  allowClear
-                  size="large"
+        <Tabs.TabPane tab={t('DVM Address')} key="address">
+          <Form layout="vertical" className="max-w-xl">
+            <Form.Item
+              label={
+                <Label
+                  text={t('DVM account')}
+                  info={
+                    <Trans t={t}>
+                      Ethereum-compatible Smart Chain Address starting with 0x. More details please refer{' '}
+                      <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://darwinianetwork.medium.com/build-on-darwinia-2-1-address-formats-in-darwinia-e964cc91fccc"
+                      >
+                        here
+                      </a>{' '}
+                      .
+                    </Trans>
+                  }
                 />
+              }
+              name="address"
+              rules={[
+                {
+                  validator(_, value) {
+                    return web3.utils.isAddress(value) ? Promise.resolve() : Promise.reject();
+                  },
+                  message: t('Address Invalid'),
+                },
+              ]}
+            >
+              <Input
+                onChange={(event) => {
+                  const value = event.target.value;
+                  const addr = web3.utils.isAddress(value) ? value : '';
+
+                  setAddress(addr);
+                }}
+                placeholder={t('DVM format account e.g.')}
+                allowClear
+                size="large"
+              />
+            </Form.Item>
+
+            {address && (
+              <Form.Item label={t('The resulting {{network}} network account id is', { network: name })}>
+                <div className="bg-white w-full rounded-lg p-4">
+                  <Typography.Text copyable>
+                    {convertToSS58(dvmAddressToAccountId(address).toString(), ss58Prefix)}
+                  </Typography.Text>
+                </div>
               </Form.Item>
+            )}
+          </Form>
+        </Tabs.TabPane>
 
-              {address && (
-                <Form.Item label={t('The resulting {{network}} network account id is', { network: name })}>
-                  <div className="bg-white w-full rounded-lg p-4">
-                    <Typography.Text copyable>
-                      {convertToSS58(dvmAddressToAccountId(address).toString(), ss58Prefix)}
-                    </Typography.Text>
-                  </div>
-                </Form.Item>
-              )}
-            </Form>
-          </Tabs.TabPane>
-        )}
-
-        {displayDvm && (
-          <Tabs.TabPane tab={t('DVM Withdraw')} key="withdraw">
-            <Withdraw />
-          </Tabs.TabPane>
-        )}
+        <Tabs.TabPane tab={t('DVM Withdraw')} key="withdraw">
+          <Withdraw />
+        </Tabs.TabPane>
 
         {name === 'darwinia' && (
           <Tabs.TabPane tab={t('Deposits Claim')} key="deposits">
