a753646af75ea73e25df83f2d1cbef7e3164b82b
JayJay1024 <lygm1062@gmail.com>
2022-08-29 18:03:53 +0800

    chore: add entity model

--- src/model/feemarket.ts
+++ src/model/feemarket.ts
@@ -215,3 +215,127 @@ export interface TFeeHistory {
     }[];
   } | null;
 }
+
+type AmountIndex = {
+  amount: string;
+  blockTime: string;
+  blockNumber: number;
+  extrinsicIndex: string | null;
+  eventIndex: string;
+};
+
+export interface QuoteEntity {
+  id: string;
+
+  relayerId: string;
+  data: [AmountIndex] | null;
+}
+
+export interface FeeEntity {
+  id: string;
+
+  marketId: string;
+  data: [AmountIndex];
+  lastTime: number;
+}
+
+export interface RewardEntity {
+  id: string;
+
+  orderId: string;
+  marketId: string;
+  relayerId: string;
+
+  blockTime: string;
+  blockNumber: string;
+  extrinsicIndex: number | null;
+  eventIndex: string;
+
+  amount: string;
+  relayerRole: RelayerRole;
+}
+
+export interface SlashEntity {
+  id: string;
+
+  orderId: string;
+  marketId: string;
+  relayerId: string;
+
+  blockTime: string;
+  blockNumber: string;
+  extrinsicIndex: number | null;
+  eventIndex: string;
+
+  amount: string;
+  relayerRole: RelayerRole;
+
+  sentTime: number;
+  confirmTime: number;
+  delayTime: number;
+}
+
+export interface MarketEntity {
+  id: string;
+
+  totalOrders: number;
+  totalSlash: string;
+  totalReward: string;
+
+  averageSpeed: number;
+
+  finishedOrders: number;
+  unfinishedInSlotOrders: number;
+  unfinishedOutOfSlotOrders: number;
+}
+
+export interface RelayerEntity {
+  id: string;
+
+  marketId: string;
+  address: string;
+
+  totalOrders: number;
+  totalSlashes: string;
+  totalRewards: string;
+}
+
+export interface OrderEntity {
+  id: string;
+
+  lane: string;
+  nonce: string;
+  marketId: string;
+
+  sender: string | null;
+  sourceTxHash: string | null;
+
+  fee: string;
+  status: OrderStatus;
+
+  slotTime: number;
+  outOfSlotBlock: number;
+  slotIndex: number | null;
+
+  createBlockTime: string;
+  createBlockNumber: number;
+  createExtrinsicIndex: number | null;
+  createEventIndex: number;
+
+  finishBlockTime: string;
+  finishBlockNumber: number;
+  finishExtrinsicIndex: number | null;
+  finishEventIndex: number;
+
+  treasuryAmount: string | null;
+  assignedRelayersAddress: [string];
+}
+
+export interface OrderRelayerEntity {
+  id: string;
+
+  orderId: string;
+  relayerId: string;
+
+  relayerRole: RelayerRole;
+}
