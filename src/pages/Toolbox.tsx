f60261f3f469aeee56ff98af97c0e7f6af8991bd
JayJay1024 <lygm1062@gmail.com>
2022-10-29 21:07:23 +0800

    feat: faucet toolbox

--- src/pages/Toolbox.tsx
+++ src/pages/Toolbox.tsx
@@ -1,25 +1,47 @@
 import { Tabs } from 'antd';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { ConvertAddress } from '../components/toolbox/address';
 import { Withdraw } from '../components/toolbox/withdraw';
 import { Deposits } from '../components/toolbox/deposits/';
 import { Faucet } from '../components/toolbox/faucet';
 import { useApi } from '../hooks';
-import { EVMChainConfig } from '../model';
+import { EVMChainConfig, SearchParamsKey } from '../model';
 import { MetamaskProvider } from '../providers/metamask';
+
+type ActiveTab = 'address' | 'faucet' | 'withdraw' | 'deposit';
 
 export function Toolbox() {
   const { t } = useTranslation();
   const { network } = useApi();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState<ActiveTab>('faucet');
 
   const { name, evm: supportEvm } = network as EVMChainConfig;
-
   const supportFaucet = useMemo(() => name === 'pangolin' || name === 'pangoro', [name]);
+
+  const handleChange = useCallback(
+    (tab: string) => {
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.set(SearchParamsKey.TAB, tab);
+      setActiveTab(tab as ActiveTab);
+      navigate(`${location.pathname}?${searchParams.toString()}`);
+    },
+    [navigate, location]
+  );
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    setActiveTab((searchParams.get(SearchParamsKey.TAB) as ActiveTab) || 'address');
+  }, []);
 
   return (
     <MetamaskProvider>
       <Tabs
+        activeKey={activeTab}
+        onChange={handleChange}
         className={`lg:px-8 px-4 w-full mx-auto dark:shadow-none dark:border-transparent pb-5 page-account-tabs page-account-tabs-${name}`}
       >
         {supportEvm && (
