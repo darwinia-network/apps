eeffb214c71ff4b94ea718678d517ceb755e2d6e
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2023-03-09 17:08:25 +0800

    fix: added account migration and staking Dapps,Updated portal order

--- src/config/network/networks.ts
+++ src/config/network/networks.ts
@@ -1,15 +1,3 @@
-import { crabConfig } from './crab';
 import { darwiniaConfig } from './darwinia';
-import { pangolinConfig } from './pangolin';
-import { pangoroConfig } from './pangoro';
-import { crabParachainConfig } from './crab-parachain';
-import { pangolinParachainConfig } from './pangolin-parachain';
 
-export const SYSTEM_NETWORK_CONFIGURATIONS = [
-  crabConfig,
-  darwiniaConfig,
-  pangolinConfig,
-  pangoroConfig,
-  crabParachainConfig,
-  pangolinParachainConfig,
-];
+export const SYSTEM_NETWORK_CONFIGURATIONS = [darwiniaConfig];
