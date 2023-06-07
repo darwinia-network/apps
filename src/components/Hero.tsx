5727d6b89e2851d7fbd7203585d23a2ae005c0b8
JayJay1024 <lygm1062@gmail.com>
2023-06-07 10:58:11 +0800

    Fix hero text on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,9 @@ export const Hero = () => {
     <div className="container mx-auto mt-[0.625rem] flex flex-col items-center justify-between gap-5 py-5 lg:mt-[1.875rem] lg:flex-row lg:gap-[1.875rem]">
       <img alt="..." src={hero} />
       <p className="text-bold block text-lg text-white lg:hidden lg:text-5xl">
-        Explore Dapps building on <span className="text-primary">Darwinia2</span> ecosystem_
+        Explore Dapps building on {"{"}
+        <span className="text-primary">Darwinia2</span>
+        {"}"} ecosystem_
       </p>
       <div className="hidden w-full lg:block">
         <p className="text-bold text-lg text-white lg:text-5xl" ref={ref} />
