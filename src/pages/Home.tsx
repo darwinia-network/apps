147246a5939eda7d374e30a98def66209904f224
JayJay1024 <lygm1062@gmail.com>
2023-07-12 11:08:13 +0800

    Add disclaimer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Disclaimer } from "../components/Disclaimer";
 import { Hero } from "../components/Hero";
 import { Portals } from "../components/Portals";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <Portals />
+      <Disclaimer />
     </>
   );
 }
