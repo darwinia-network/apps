a2c03a53c9732467f9fd95168725ea6946711037
Jordy-Baby <82444671+Jordy-Baby@users.noreply.github.com>
2024-06-20 13:48:16 +0100

    Update projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -48,7 +48,7 @@ import subApiDao from "../assets/projects/subapi-dao.png";
 import helixDao from "../assets/projects/helix-dao.png";
 import immunefi from "../assets/projects/immunefi.png";
 import ktonStaking from "../assets/projects/kton-staking.png";
-import ktonStaking from "..assets/projects/Envio_logo_sqaure.png";
+import envio from "..assets/projects/Envio_logo_sqaure.png";
 
 export const projects = (t: TFunction): PortalMeta[] => [
   {
